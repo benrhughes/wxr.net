af6ceb9bd3048f8619b21e0017c186eb83035cbb
Ben <ben@benrhughes.com>
2012-08-11 23:45:50 +1000

    initial

---
+++ wxr.net/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WXR
+{
+	public class Post
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Content { get; set; }
+		public string Id { get; set; }
+		public DateTime Date { get; set; }
+		public string Status { get; set; }
+
+		public IEnumerable<string> Tags { get; set; }
+		public IEnumerable<string> Categories { get; set; }
+
+		public Post()
+		{
+			this.Tags = new List<string>();
+		}
+	}
+}
---
+++ wxr.net/Site.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace WXR
+{
+	public class Site
+	{
+		public string Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public IList<Post> Posts { get; set; }
+		public IEnumerable<string> Tags { get; set; }
+		public IEnumerable<string> Categories { get; set; }
+
+		public Site()
+		{
+			Posts = new List<Post>();
+		}
+
+		public override string ToString()
+		{
+			return Title;
+		}
+
+		public XmlDocument Serialize()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void DownloadMedia(string targetDir)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object SerializeToDisk(string targetDir)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
